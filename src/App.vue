7dc0a1cbffacf4b6e8fa203f757930d755644d00
leongxj123 <113355197+leongxj123@users.noreply.github.com>
2024-01-07 03:03:03 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -46,10 +46,10 @@ export default {
     let titleTime
     document.addEventListener('visibilitychange', function () {
       if (document.hidden) {
-        document.title = '死鬼去哪里了！'
+        document.title = '在线播放！'
         clearTimeout(titleTime)
       } else {
-        document.title = '(つェ⊂)咦!又好了!'
+        document.title = '(つェ⊂)好了!'
         titleTime = setTimeout(function () {
           document.title = OriginTitile
         }, 2000)
