9452ae1c3ff2dd0250ea5bcd60cf9b2f6a0bee73
leongxj123 <113355197+leongxj123@users.noreply.github.com>
2024-01-07 03:00:57 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -39,16 +39,14 @@ router.beforeEach((to, from, next) => {
     next()
   } else {
     document.title =
-      (to.meta.title && `${to.meta.title} - mmPlayer在线音乐播放器`) || 'mmPlayer在线音乐播放器'
+      (to.meta.title && `${to.meta.title} - 在线音乐播放器`) || '在线音乐播放器'
     next()
   }
 })
 
 // 版权信息
-window.mmPlayer = window.mmplayer = `欢迎使用 mmPlayer!
+window.mmPlayer = window.mmplayer = `欢迎使用!
 当前版本为：V${VERSION}
-作者：茂茂
-Github：https://github.com/maomao1996/Vue-mmPlayer
 歌曲来源于网易云音乐 (https://music.163.com)`
 // eslint-disable-next-line no-console
 console.info(`%c${window.mmplayer}`, `color:blue`)
